74b6b38606678869b36fd0248dd41b72c6d75047
wuxw <wuxw7@asiainfo.com>
2016-10-08 17:40:03 +0800

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/14/602f89f77b8b00161bd3e7484421adf9
@@ -0,0 +1,13 @@
+package com.java110.UserRegister;
+
+public class Test {
+
+	/**
+	 * 测试
+	 * @param name
+	 * @return
+	 */
+	public String sayHelloMethod(String name){
+		return "hello " + name; 
+	}
+}
---
+++ UserRegister/target/classes/src/main/java/com/java110/UserRegister/App.java
@@ -0,0 +1,16 @@
+package com.java110.UserRegister;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+    {
+    	Test test = new Test();
+    	String returnStr = test.sayHelloMethod("wuxw");
+    	System.out.println(returnStr);
+    }
+}
